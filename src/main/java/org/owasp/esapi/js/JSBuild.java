9b68e0271ed8776d19ae377093c32b99dec699ce
chrisisbeef <chrisisbeef@5b090034-d4a3-11de-a7eb-d1396c3fe4e6>
2009-11-30 07:31:25 +0000

    Project Structure Creation - Initial checkin

---
+++ src/main/java/org/owasp/esapi/js/JSBuild.java
@@ -0,0 +1,20 @@
+/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2008 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ */
+
+package org.owasp.esapi.js;
+
+public class JSBuild {
+    public static void main( String... args ) {
+        System.out.println("Coming Soon!");
+    }
+}
