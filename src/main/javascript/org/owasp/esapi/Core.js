0a3a1eb981f6af9bc04a01da381db67062b62d45
chrisisbeef <chrisisbeef@5b090034-d4a3-11de-a7eb-d1396c3fe4e6>
2010-01-07 05:18:19 +0000

    Getting things rolling

--- src/main/javascript/org/owasp/esapi/Core.js
+++ src/main/javascript/org/owasp/esapi/Core.js
@@ -11,30 +11,93 @@
  * LICENSE before you use, modify, and/or redistribute this software.
  */
 
-@Module(Core)
+// @Module(Core)
+
+if (!$) {
+    /**
+     * Shortcut to <pre>document.getElementById</pre>
+     * @param sElementID
+     *              The ID of the element to retrieve
+     * @return HTMLObject
+     */
+    var $ = function( sElementID ) {
+        return document.getElementById( sElementID );
+    };
+}
+
+if (!Array.prototype.each) {
+    /**
+     * Iterator function for Arrays.
+     * @param fIterator The iterator function to be executed on each element in the array.
+     */
+    Array.prototype.each = function(fIterator) {
+        if (typeof fIterator != 'function') {
+            throw 'Illegal Argument for Array.each';
+        }
+
+        for (var i = 0; i < this.length; i ++) {
+            fIterator(this[i]);
+        }
+    };
+}
+
+if (!Array.prototype.contains) {
+    /**
+     * Determines whether the passed in object exists in the array.
+     * @param srch The object to search for
+     * @return True if the passed in object is found, false otherwise
+     */
+    Array.prototype.contains = function(srch) {
+        this.each(function(e) {
+            if (e === srch) {
+                return true;
+            }
+        });
+        return false;
+    };
+}
+
+$namespace('org.owasp.esapi');
 
 /**
- * Creates a new empty namespace if it doesn't already exist.
- * @param ns    The new namespace to create
- * @param sep   The seperator for the namespace (default is .)
- * @return      The namespace created
+ * DOMUtilities is a collection of helper methods for accessing the DOM. This is primarily for use between ESAPI
+ * objects, however is accessible to code outside of the ESAPI. While fairly limited, it provides simple shortcut
+ * methods to common DOM functions.
+ *
+ * @author  Chris Schmidt (chrisisbeef@gmail.com)
+ * @since   ESAPI 1.0
  */
-var $namespace = function( ns, sep ) {
-    var names = ns.split( sep || '.' ), o = window, i, len;
-    for ( i = 0, len = names.split; i < len; i ++ ) {
-        o = o[names[i]] = o[ns[i]] || {};
-    }
-    return o;
+org.owasp.esapi.DOMUtilities = function() {
+
 };
 
-/**
- * Iterator function for Arrays.
- * @param fIterator The iterator function to be executed on each element in the array.
- */
-Array.prototype.each = function( fIterator ) {
-    if ( typeof fIterator != 'function' ) { throw 'Illegal Argument for Array.each'; }
+org.owasp.esapi.ESAPI = function() {
 
-    for ( var i=0; i < this.length; i ++ ) {
-        fIterator(this[i]);
-    }
+};
+
+org.owasp.esapi.PreparedString = function() {
+    /* private */
+
+    /**
+     * This is the placeholder that will be used to mark a parameter
+     */
+    var ch_parameterCharacter = '?';
+
+    /**
+     * The {@link org.owasp.esapi.Codec} that will be used to encode values for parameters passed in
+     */
+    var o_codec = null;
+
+    /**
+     * A working list of parameters
+     */
+    var a_s_parameters = [];
+
+    var a_parts = [];
+
+    
+};
+
+org.owasp.esapi.StringUtilities = function() {
+
 };
