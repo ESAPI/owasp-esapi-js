44b207f2488d9982b888143e1debf271606ef3e3
chrisisbeef <chrisisbeef@5b090034-d4a3-11de-a7eb-d1396c3fe4e6>
2010-01-24 07:09:46 +0000

    Release 0.1.3

---
+++ dist/build-properties.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href="http://www.owasp.org/index.php/ESAPI">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2008 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ */
+
+$DOCUMENTATION_DIR = "documentation";
+$SOURCE_DIR = "src/main/javascript";
+$RESOURCES_DIR = "src/main/resources";
+$LIB_DIR = "lib";
+$OUTPUT_DIR = "dist/";
+$OUTPUT_RESOURCES = "resources/";
+$OUTPUT_DOCUMENTATION = "docs/";
+$OUTPUT_LIBS = "lib/";
+$OUTPUT_FILE = "esapi.js";
+$OUTPUT_FILE_COMPRESSED = "esapi-compressed.js";
+$OUTPUT_SRC = "src/";
+
+$JDK_HOME = "/home/cschmidt/jdk1.6.0_14/";
+$YUI_COMPRESSOR_JAR = "lib/yuicompressor-2.4.2.jar";
+$TMP_DIR = "tmp/";
+
+$LICENSE_TEXT =
+"/*
+ * OWASP Enterprise Security API (ESAPI)
+ *
+ * This file is part of the Open Web Application Security Project (OWASP)
+ * Enterprise Security API (ESAPI) project. For details, please see
+ * <a href=\"http://www.owasp.org/index.php/ESAPI\">http://www.owasp.org/index.php/ESAPI</a>.
+ *
+ * Copyright (c) 2008 - The OWASP Foundation
+ *
+ * The ESAPI is published by OWASP under the BSD license. You should read and accept the
+ * LICENSE before you use, modify, and/or redistribute this software.
+ */\n";
+?>
